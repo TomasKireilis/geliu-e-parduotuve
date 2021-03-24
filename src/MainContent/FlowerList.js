2a1c964e07f88a1e9b5aabea2cd10ea29c11370d
Tomas <t.kireilis@gmail.com>
2021-03-24 16:42:15 +0200

    Added sorting functionality and updated naming and data

--- src/MainContent/FlowerList.js
+++ src/MainContent/FlowerList.js
@@ -6,24 +6,85 @@ import FlowerListFilters from "./FlowerListFilters";
 
 function FlowerList() {
   const data = [
-    { imgSrc: "flowersIcon.jpg", name: "Palergonija", info: "info" },
-    { imgSrc: "flowersIcon.jpg", name: "Litva", info: "info" },
-    { imgSrc: "flowersIcon.jpg", name: "Lietuva", info: "info" },
-    { imgSrc: "flowersIcon.jpg", name: "Papa", info: "info" },
+    {
+      imgSrc: "flowersIcon.jpg",
+      name: "Palergonija",
+      info: "info",
+      price: 5,
+      type: "flowerBouquet",
+    },
+    {
+      imgSrc: "flowersIcon.jpg",
+      name: "a",
+      info: "info",
+      price: 2,
+      type: "flowerBouquet",
+    },
+    {
+      imgSrc: "flowersIcon.jpg",
+      name: "b",
+      info: "info",
+      price: 4,
+      type: "flower",
+    },
+    {
+      imgSrc: "flowersIcon.jpg",
+      name: "c",
+      info: "info",
+      price: 15,
+      type: "flower",
+    },
+    {
+      imgSrc: "flowersIcon.jpg",
+      name: "Litva",
+      info: "info",
+      price: 1,
+      type: "flowerBouquet",
+    },
+    {
+      imgSrc: "flowersIcon.jpg",
+      name: "Lietuva",
+      info: "info",
+      price: 5,
+      type: "flower",
+    },
+    {
+      imgSrc: "flowersIcon.jpg",
+      name: "Papa",
+      info: "info",
+      price: 9,
+      type: "flower",
+    },
   ];
-
+  const getProductsPriceRange = () => {
+    const prices = data.map((x) => x.price);
+    return { min: Math.min(...prices), max: Math.max(...prices) };
+  };
   const [filteredFlowerList, setFilteredFlowerList] = useState(data);
-  const handleChange = (event) => {
-    setFilteredFlowerList(
-      data.filter((x) =>
-        x.name.toLowerCase().match(event.target.value.toLowerCase())
-      )
-    );
+  const handleChange = (value) => {
+    var filteringData = JSON.parse(JSON.stringify(data));
+    if (value.name && value.name != "") {
+      filteringData = filteringData.filter((x) =>
+        x.name.toLowerCase().match(value.name)
+      );
+    }
+    if (value.price) {
+      filteringData = filteringData.filter(
+        (x) => x.price >= value.price.min && x.price <= value.price.max
+      );
+    }
+    if (value.type) {
+      filteringData = filteringData.filter((x) => x.type === value.type);
+    }
+    setFilteredFlowerList(filteringData);
   };
   return (
     <Col>
       <Row>
-        <FlowerListFilters handleChange={handleChange} />
+        <FlowerListFilters
+          priceRange={getProductsPriceRange}
+          handleChange={handleChange}
+        />
       </Row>
       <Row>
         <div className="flower-list">
--- src/MainContent/FlowerListFilters.js
+++ src/MainContent/FlowerListFilters.js
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Container, Form, Row } from "react-bootstrap";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
 import InputRange from "react-input-range";
 import "react-input-range/lib/css/index.css";
-function FlowerListFilters({ handleChange }) {
-  const [state, setState] = useState(5);
-  const [state4, setState4] = useState({ min: 5, max: 10 });
+function FlowerListFilters({ priceRange, handleChange }) {
+  const [currentPriceValues, setCurrentPriceValues] = useState(priceRange());
+  const [currentType, setcurrentType] = useState(0);
+  const [currentName, setCurrentName] = useState(0);
+  const updateNameFilter = (event) => {
+    setCurrentName(event.target.value.toLowerCase());
+  };
+  const updateTypeFilter = (event) => {
+    setcurrentType(event.target.value);
+  };
+  useEffect(() => {
+    handleChange({
+      price: currentPriceValues,
+      type: currentType,
+      name: currentName,
+    });
+  }, [currentPriceValues, currentType, currentName]);
   return (
     <div className="flower-list">
       <Container className="flower-list-container">
@@ -16,26 +30,27 @@ function FlowerListFilters({ handleChange }) {
               as="select"
               className="my-1 mr-sm-2"
               id="inlineFormCustomSelectPref"
+              onChange={updateTypeFilter}
               custom
             >
-              <option value="0">Visi tipai</option>
-              <option value="1">Gėlės</option>
-              <option value="2">Puokštės</option>
+              <option value="">Visi tipai</option>
+              <option value="flower">Gėlės</option>
+              <option value="flowerBouquet">Puokštės</option>
             </Form.Control>
           </div>
           <div className="input-range-container">
             <InputRange
-              maxValue={20}
-              minValue={0}
+              maxValue={priceRange().max}
+              minValue={priceRange().min}
               formatLabel={(value) => `${value} eur`}
-              value={state4}
-              onChange={(value) => setState4(value)}
+              value={currentPriceValues}
+              onChange={(value) => setCurrentPriceValues(value)}
               onChangeComplete={(value) => console.log(value)}
               className="input-range"
             />
           </div>
         </DropdownButton>
-        <Form.Control placeholder="Ieškoti vardo" onChange={handleChange} />
+        <Form.Control placeholder="Ieškoti vardo" onChange={updateNameFilter} />
       </Container>
     </div>
   );
