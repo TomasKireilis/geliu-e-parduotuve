34d5459216b47c9d0fe5069f42a143593b83b4de
Tomas <t.kireilis@gmail.com>
2021-03-24 09:23:08 +0200

    Added localdb save

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,13 +1,18 @@
-function AppReducer (state, action) {
-    switch (action.type) {
-    case 'DELETE_ITEM':
-        return {
-            ...state,
-            cartItems: state.cartItems.filter((item) => item.id !== action.payload),
-        };
+function AppReducer(state, action) {
+  switch (action.type) {
+    case "DELETE_ITEM":
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((item) => item.id !== action.payload),
+      };
+    case "ADD_BASKET":
+      return {
+        ...state,
+        cartItems: action.payload,
+      };
     default:
-        return state;
-    }
-  };
+      return state;
+  }
+}
 
-  export default AppReducer;
+export default AppReducer;
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,38 +1,71 @@
-import React, { createContext, useReducer } from 'react';
-import AppReducer from './AppReducer';
+import React, { createContext, useEffect, useReducer } from "react";
+import AppReducer from "./AppReducer";
 
 const initialState = {
-  cartItems: [ { id: 1, image: 'https://images.unsplash.com/photo-1554631221-f9603e6808be?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
-    title: 'Kaktusas', price: 5.99, amount: 4 },
-    { id: 2, image: 'https://www.ikea.com/gb/en/images/products/smycka-artificial-flower-rose-red__0636963_pe698124_s5.jpg', title: 'Rožės', price: 25.50, amount: 2 },
-    { id: 3, image: 'https://i.pinimg.com/564x/18/4b/61/184b61bfe08b19ee4f4e07d605673d6f.jpg', title: 'Prabangi puokste', price: 100, amount: 1 },
-    ]
+  cartItems: [
+    {
+      id: 1,
+      image:
+        "https://images.unsplash.com/photo-1554631221-f9603e6808be?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+      title: "Kaktusas",
+      price: 5.99,
+      amount: 4,
+    },
+    {
+      id: 2,
+      image:
+        "https://www.ikea.com/gb/en/images/products/smycka-artificial-flower-rose-red__0636963_pe698124_s5.jpg",
+      title: "Rožės",
+      price: 25.5,
+      amount: 2,
+    },
+    {
+      id: 3,
+      image:
+        "https://i.pinimg.com/564x/18/4b/61/184b61bfe08b19ee4f4e07d605673d6f.jpg",
+      title: "Prabangi puokste",
+      price: 100,
+      amount: 1,
+    },
+  ],
 };
 
 export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(AppReducer, initialState);
+  const [cartItems, dispatchCartItems] = useReducer(AppReducer, initialState);
+  function deleteItem(id) {
+    try {
+      dispatchCartItems({
+        type: "DELETE_ITEM",
+        payload: id,
+      });
+    } catch (error) {}
+  }
 
-    function deleteItem(id) {
+  useEffect(() => {
+    const data = localStorage.getItem("my-shopping-cart");
+    if (data) {
       try {
-          dispatch({
-            type: 'DELETE_ITEM',
-            payload: id,
+        dispatchCartItems({
+          type: "ADD_BASKET",
+          payload: JSON.parse(data),
         });
-      }
-       catch (error) {
-       }
+      } catch (error) {}
     }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("my-shopping-cart", JSON.stringify(cartItems));
+  }, [cartItems]);
 
   return (
     <GlobalContext.Provider
       value={{
-        cartItems: state.cartItems,
-        deleteItem
-      }}  
+        cartItems: cartItems.cartItems,
+        deleteItem,
+      }}
     >
       {children}
     </GlobalContext.Provider>
   );
-};  
+};
