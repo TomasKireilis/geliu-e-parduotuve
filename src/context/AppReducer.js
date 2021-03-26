5d0d0d5a8e3a2df42cb59a7e585cee5886bf5dcf
Tomas <t.kireilis@gmail.com>
2021-03-26 20:38:30 +0200

    Changed for to index

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -11,12 +11,10 @@ function AppReducer(state, action) {
         cartItems: action.payload,
       };
     case "UPDATE_ITEM_AMOUNT": {
-      for (var i in state.cartItems) {
-        if (state.cartItems[i].id === action.payload.id) {
-          state.cartItems[i].amount = action.payload.amount;
-          break;
-        }
-      }
+      const index = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      state.cartItems[index].amount = action.payload.amount;
       return {
         ...state,
         cartItems: [...state.cartItems],
