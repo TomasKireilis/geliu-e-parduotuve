75d2c91dfe0bb630dc862b3d4a31f30580b4564f
Tomas <t.kireilis@gmail.com>
2021-05-25 15:08:01 +0300

    Initial

---
+++ src/ShoppingCartPage/IntegrationServicePage.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import ShoppingCartTable from "./ShoppingCartTable";
+import ShoppingCartNote from "./ShoppingCartNote";
+import ShoppingCartTotalField from "./ShoppingCartTotalField";
+import { Row, Container, Col } from "react-bootstrap";
+import {} from "react-bootstrap/Container";
+import ShoppingCartOrderButton from "./ShoppingCartOrderButton";
+
+function IntegrationServicePage({ updateHeaderTitle }) {
+  useEffect(() => {
+    updateHeaderTitle("Pirkinių krepšelis");
+  }, []);
+
+  return (
+    <>
+      <div className="basket-content">
+        <Container fluid>
+          <Row>
+            <ShoppingCartTable />
+          </Row>
+          <Row>
+            <ShoppingCartTotalField />
+          </Row>
+          <Row>
+            <ShoppingCartNote />
+          </Row>
+          <Row>
+            <ShoppingCartOrderButton />
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default IntegrationServicePage;
