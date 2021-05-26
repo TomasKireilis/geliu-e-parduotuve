853b6d76b94911cba83b16d1fe8834d77f7ccdb3
Tomas <t.kireilis@gmail.com>
2021-05-26 17:28:56 +0300

    Fixed admin to show something, but there is deeper problem

--- src/App.js
+++ src/App.js
@@ -5,29 +5,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "Header/Header";
 import ShoppingCartPage from "./ShoppingCartPage/ShoppingCartPage";
 import IntegrationServicePage from "IntegrationService/IntegrationServicePage";
-import { useState, useContext, useEffect } from "react";
+import { useState } from "react";
 import { GlobalProvider } from "Context/GlobalState";
-
-import { checkCurrentUserRole } from "Service/FlowerService";
-import { GlobalContext } from "Context/GlobalState.js";
 
 function App() {
   const [headerTitle, setHeaderTitle] = useState("Gėlių e-parduotuvė");
   const updateHeaderTitle = (title) => {
     setHeaderTitle(title);
   };
-  const { loginInfo } = useContext(GlobalContext);
-  const [userType, setUserType] = useState("anomymous");
-
-  useEffect(async () => {
-    setUserType(
-      await checkCurrentUserRole({
-        loggedIn: loginInfo.loggedIn,
-        email: loginInfo.email,
-        password: loginInfo.password,
-      })
-    );
-  }, [loginInfo]);
 
   return (
     <GlobalProvider>
@@ -53,17 +38,16 @@ function App() {
                     <ShoppingCartPage updateHeaderTitle={updateHeaderTitle} />
                   )}
                 />
-                {userType != "anomymous" && (
-                  <Route
-                    exact
-                    path="/IntegrationService"
-                    component={() => (
-                      <IntegrationServicePage
-                        updateHeaderTitle={updateHeaderTitle}
-                      />
-                    )}
-                  />
-                )}
+
+                <Route
+                  exact
+                  path="/IntegrationService"
+                  component={() => (
+                    <IntegrationServicePage
+                      updateHeaderTitle={updateHeaderTitle}
+                    />
+                  )}
+                />
               </div>
             </div>
           </div>
--- src/IntegrationService/IntegrationServicePage.js
+++ src/IntegrationService/IntegrationServicePage.js
@@ -8,10 +8,6 @@ function IntegrationServicePage({ updateHeaderTitle }) {
   const { loginInfo } = useContext(GlobalContext);
   const [userType, setUserType] = useState("anomymous");
 
-  useEffect(() => {
-    updateHeaderTitle("Integracinis servisas");
-  }, []);
-
   useEffect(async () => {
     setUserType(
       await checkCurrentUserRole({
@@ -22,54 +18,59 @@ function IntegrationServicePage({ updateHeaderTitle }) {
     );
   }, [loginInfo]);
 
+  useEffect(() => {
+    updateHeaderTitle("Integracinis servisas");
+  }, []);
   const [basketPrice, setBasketPrice] = useState(0);
 
   return (
     <>
-      <div className="basket-content">
-        <Container fluid>
-          <Row>
-            <Col style={{ maxWidth: "100%" }}>
-              <Row style={{ marginTop: "5px", marginBottom: "5px" }}>
-                <Button className="login-button">Prekių užsakymas</Button>
-              </Row>
-            </Col>
-          </Row>
-          <div className="page-not-available">
+      {userType == "admin" && (
+        <div className="basket-content">
+          <Container fluid>
             <Row>
-              <div className="development-title">
-                This page is being developed. Come back soon to see new
-                features.
+              <Col style={{ maxWidth: "100%" }}>
+                <Row style={{ marginTop: "5px", marginBottom: "5px" }}>
+                  <Button className="login-button">Prekių užsakymas</Button>
+                </Row>
+              </Col>
+            </Row>
+            <div className="page-not-available">
+              <Row>
+                <div className="development-title">
+                  This page is being developed. Come back soon to see new
+                  features.
+                </div>
+              </Row>
+              <Row>
+                <img
+                  className="under-development--img--inner absolute-center "
+                  src="UnderDevelopmentInnerPart.png"
+                />
+
+                <img
+                  className="under-development-img-outer"
+                  src="UnderDevelopmentOuterPart.png"
+                />
+              </Row>
+            </div>
+
+            {/* <Row>
+              <IntegrationServiceBuyTable
+                setBasketPrice={(value) => setBasketPrice(value)}
+              />
+            </Row> */}
+            <Row>
+              <div className="basket-total-container">
+                Bendra suma: {basketPrice.toFixed(2)} EUR
               </div>
             </Row>
             <Row>
-              <img
-                className="under-development--img--inner absolute-center "
-                src="UnderDevelopmentInnerPart.png"
-              />
-
-              <img
-                className="under-development-img-outer"
-                src="UnderDevelopmentOuterPart.png"
-              />
+              <Button className="order-button">Užsakyti</Button>
             </Row>
-          </div>
-
-          <Row>
-            <IntegrationServiceBuyTable
-              setBasketPrice={(value) => setBasketPrice(value)}
-            />
-          </Row>
-          <Row>
-            <div className="basket-total-container">
-              Bendra suma: {basketPrice.toFixed(2)} EUR
-            </div>
-          </Row>
-          <Row>
-            <Button className="order-button">Užsakyti</Button>
-          </Row>
-        </Container>
-      </div>
+          </Container>
+        </div>
+      )}
     </>
   );
 }
